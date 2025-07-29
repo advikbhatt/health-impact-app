906cca01d0625b5e0b188cbf14a7d404d379fadc
Advik Bhatt <advikbhatt24@gmail.com>
2025-07-29 21:01:51 +0530

    render push

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,23 @@ from firebase_admin import credentials, firestore
 
 # Load environment variables
 load_dotenv()
-
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+})
+firebase_admin.initialize_app(cred)
 # Initialize Firebase only once
-if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_config.json")
-    firebase_admin.initialize_app(cred)
+# if not firebase_admin._apps:
+#     cred = credentials.Certificate("firebase_config.json")
+#     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
